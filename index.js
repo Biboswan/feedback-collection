bac7a5ef75bbe56ebba542867cd5f5397e3b9af5
Biboswan Roy <biboswan98@gmail.com>
2018-08-04 14:49:34 +0530

    Fnish auth flow

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+
+const keys = require('./config/keys');
+require('./models/Users');
+require('./services/passport'); // order of require matter
+
+mongoose.connect(keys.mongoURI);
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({ hi: 'there'})
-});
+app.use(cookieSession({
+	maxAge: 30 * 60 * 60 * 1000,
+	keys: [keys.cookieKey],
+})
+);
+app.use(passport.initialize()).use(passport.session());
+
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
