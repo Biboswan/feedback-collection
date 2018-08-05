682d31ddf3138996c72a8d4160186738352f3d8d
Biboswan Roy <biboswan98@gmail.com>
2018-08-05 22:54:30 +0530

    Implement add credit feature

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
+const bodyParser = require('body-parser');
 
 const keys = require('./config/keys');
 require('./models/Users');
@@ -11,6 +12,7 @@ mongoose.connect(keys.mongoURI);
 
 const app = express();
 
+app.use(bodyParser.json());
 app.use(cookieSession({
 	maxAge: 30 * 60 * 60 * 1000,
 	keys: [keys.cookieKey],
@@ -20,6 +22,18 @@ app.use(cookieSession({
 app.use(passport.initialize()).use(passport.session());
 
 require('./routes/authRoutes')(app);
+require('./routes/billingRoutes')(app);
+
+if (process.env.NODE_ENV === 'production') {
+	// production will serve up production assets 
+	// like our main.js file, or main.css file!
+	app.use(express.static('client/build'));
+
+	const path = require('path');
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
